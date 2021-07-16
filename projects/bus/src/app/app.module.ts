c4bd50347fc539f488a79f929f3e66baf3b93845
Jawade Dhondiram Nivrati <dhondiram.jawade@yash.com>
2021-07-16 19:53:15 +0530

    Angular Material Added

--- projects/bus/src/app/app.module.ts
+++ projects/bus/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { CoreModule } from 'projects/core/src/public-api';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     RouterModule,
-    CoreModule
+    CoreModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
