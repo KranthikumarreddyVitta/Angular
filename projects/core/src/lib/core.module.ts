f36542168277fa111d93346518b8e74f440dcfb5
Jawade Dhondiram Nivrati <dhondiram.jawade@yash.com>
2021-07-19 17:20:04 +0530

    changed schematics for css

--- projects/core/src/lib/core.module.ts
+++ projects/core/src/lib/core.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { ImageComponent } from './components/image/image.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    ImageComponent
   ],
   imports: [
   ],
