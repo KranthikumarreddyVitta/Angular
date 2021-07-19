ffc41e51718ab6c849d63fb3507a6d1e468d2a08
Jawade Dhondiram Nivrati <dhondiram.jawade@yash.com>
2021-07-19 21:38:45 +0530

    Header Feature partialy commited

---
+++ projects/core/src/lib/components/navbar/inavbar.ts
@@ -0,0 +1,5 @@
+export interface INavItem {
+    name:string,
+    route : string,
+    isActive : boolean;
+}
--- projects/core/src/lib/core.module.ts
+++ projects/core/src/lib/core.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { HeaderComponent } from './components/header/header.component';
 import { ImageComponent } from './components/image/image.component';
-
+import { MatMenuModule } from "@angular/material/menu";
+import { MatButtonModule } from "@angular/material/button";
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    ImageComponent
+    ImageComponent,
+    NavbarComponent
   ],
   imports: [
+    FlexLayoutModule,
+    MatMenuModule,
+    MatButtonModule,
+    BrowserModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    NavbarComponent
   ]
 })
 export class CoreModule { }
