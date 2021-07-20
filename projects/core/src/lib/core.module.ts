ffffd4a423172e02ba6a3b164b0880df71fa20a5
Jitendra Gupta <jitendra@inhabitr.in>
2021-07-20 15:00:47 +0530

    create tabs, banner & cart component

--- projects/core/src/lib/core.module.ts
+++ projects/core/src/lib/core.module.ts
@@ -6,13 +6,19 @@ import { MatMenuModule } from "@angular/material/menu";
 import { MatButtonModule } from "@angular/material/button";
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { BannerComponent } from './components/banner/banner.component';
+import { TabsComponent } from './components/tabs/tabs.component';
+import { CartComponent } from './components/cart/cart.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
     ImageComponent,
-    NavbarComponent
+    NavbarComponent,
+    BannerComponent,
+    TabsComponent,
+    CartComponent
   ],
   imports: [
     FlexLayoutModule,
@@ -22,7 +28,10 @@ import { BrowserModule } from '@angular/platform-browser';
   ],
   exports: [
     HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    BannerComponent,
+    TabsComponent,
+    CartComponent
   ]
 })
 export class CoreModule { }
