62e5a1ad1aaa536ecf908c95b9bde96482f105fd
Jitendra Gupta <jitendra@inhabitr.in>
2021-07-30 20:50:05 +0530

    MB Details header

--- projects/moodboard/src/lib/components/moodboard/moodboard.component.ts
+++ projects/moodboard/src/lib/components/moodboard/moodboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MoodboardService } from '../../services/moodboard.service';
 
 @Component({
   selector: 'lib-moodboard',
@@ -6,10 +8,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./moodboard.component.scss']
 })
 export class MoodboardComponent implements OnInit {
-
-  constructor() { }
-
+  public mbId: any = '';
+  constructor(private moodboardService:MoodboardService, private activatedRoute: ActivatedRoute) { 
+     this.mbId = this.activatedRoute.snapshot.paramMap.get('id');
+  }
+  bannerIconImg: any = 'assets/moodboard/images/mb.png';
+  bannerIconImgTxt: any = 'Moodboard';
+  moodboardDetails: any = '';
+  catagorydata = [{
+    imageSrc: 'assets/moodboard/images/Categories-01.png',
+    value: 'Living Room',
+  },
+  {
+    imageSrc: 'assets/moodboard/images/Categories-02.png',
+    value: 'Bedroom',
+  },
+  {
+    imageSrc: 'assets/moodboard/images/Categories-03.png',
+    value: 'Dining Room',
+  },
+  {
+    imageSrc: 'assets/moodboard/images/Categories-04.png',
+    value: 'Office',
+  },
+  {
+    imageSrc: 'assets/moodboard/images/Categories-05.png',
+    value: 'Outdoor',
+  },
+  {
+    imageSrc: 'assets/moodboard/images/Categories-06.png',
+    value: 'Others',
+  }
+]
   ngOnInit(): void {
+    this.getMoodboard();
+  }
+
+  getMoodboard(){
+    this.moodboardService.getMoodBoard(this.mbId).subscribe((response:any) => {
+      this.moodboardDetails = response;
+    });    
   }
 
 }
--- projects/moodboard/src/lib/services/moodboard.service.ts
+++ projects/moodboard/src/lib/services/moodboard.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { EnvironmentService } from 'projects/core/src/lib/services/environment.service';
 import { HttpService } from 'projects/core/src/public-api';
 import { Observable } from 'rxjs';
 
@@ -8,25 +9,30 @@ import { Observable } from 'rxjs';
 })
 export class MoodboardService {
 
-  constructor(private http: HttpService) { }
+  constructor(private http: HttpService,private env: EnvironmentService) { }
+  
   getStateList<T>(): Observable<T> {
-    let url = 'load/states';
+    let url = this.env.getEndPoint()+'load/states';
     return this.http.sendGETRequest(url, {});
   }
   getProjectList<T>(): Observable<T> {
-    let url = 'getMoodBoardProjectFilter?project_name=';
+    let url = this.env.getEndPoint()+'getMoodBoardProjectFilter?project_name=';
+    return this.http.sendGETRequest(url, {});
+  }
+  getMoodBoard<T>(id: any): Observable<T> {
+    let url = this.env.getEndPoint()+'load/moodboard/items?moodboard_id='+id;
     return this.http.sendGETRequest(url, {});
   }
   getMoodBoardList<T>(): Observable<T> {
-    let url = 'getMoodBoard?supplier_id=0&project_name=&user_id=98';
+    let url = this.env.getEndPoint()+'getMoodBoard?supplier_id=0&project_name=&user_id=98';
     return this.http.sendGETRequest(url, {});
   }
   getMyMoodBoardList<T>(): Observable<T> {
-    let url = 'getMoodBoardByUser?supplier_id=0&project_name=&user_id=98';
+    let url = this.env.getEndPoint()+'getMoodBoardByUser?supplier_id=0&project_name=&user_id=98';
     return this.http.sendGETRequest(url, {});
   }
   createMoodboard(param:any): Observable <any> {
-    let url = 'createMoodBoard'
+    let url = this.env.getEndPoint()+'createMoodBoard'
     return this.http.sendPOSTRequest<any>(url,param,{});
   }
 }
