c1ee8e37bddc4a6901c6380ad2f1b96c227151b3
Jawade Dhondiram Nivrati <dhondiram.jawade@yash.com>
2022-01-13 18:37:07 +0530

    dev build

--- projects/core/src/lib/services/http.service.ts
+++ projects/core/src/lib/services/http.service.ts
@@ -1,46 +1,51 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 export interface IHttpOptions {
-  headers?: HttpHeaders | {
-    [header: string]: string | string[];
-};
-observe?: any;
-params?: HttpParams | {
-    [param: string]: string | string[];
-};
-reportProgress?: boolean;
-responseType?: any;
-withCredentials?: boolean;
+  headers?:
+    | HttpHeaders
+    | {
+        [header: string]: string | string[];
+      };
+  observe?: any;
+  params?:
+    | HttpParams
+    | {
+        [param: string]: string | string[];
+      };
+  reportProgress?: boolean;
+  responseType?: any;
+  withCredentials?: boolean;
 }
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class HttpService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   /**
    * Send Get Request
-   * @param url 
-   * @param httpOptions 
-   * @returns 
+   * @param url
+   * @param httpOptions
+   * @returns
    */
-  sendGETRequest<T>(url: string,httpOptions?: IHttpOptions ) : Observable<T>{
-    return this.http.get<T>(url,httpOptions);
+  sendGETRequest<T>(url: string, httpOptions?: IHttpOptions): Observable<T> {
+    return this.http.get<T>(url, httpOptions);
   }
 
   /**
    * Send Post Request
-   * @param url 
-   * @param Json 
-   * @param httpOptions 
-   * @returns 
+   * @param url
+   * @param Json
+   * @param httpOptions
+   * @returns
    */
-  sendPOSTRequest<T>(url:string,Json: string,httpOptions? :IHttpOptions): Observable<T>{
-    return this.http.post<T>(url,Json,{headers : {'Access-Control-Allow-Origin':'*'}});
+  sendPOSTRequest<T>(
+    url: string,
+    Json: string,
+    httpOptions?: IHttpOptions
+  ): Observable<T> {
+    return this.http.post<T>(url, Json);
   }
 }
